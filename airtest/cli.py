50e6b6b163cae79e3c8e80b77418f9d5b32f9f8d
codeskyblue <codeskyblue@gmail.com>
2014-06-24 20:01:23 +0800

    add skip install

--- airtest/cli.py
+++ airtest/cli.py
@@ -4,12 +4,13 @@
 
 '''
 Usage:
-    air.test [-c FILE] [-s SERIALNO]
+    air.test [--skip-install] [-c FILE] [-s SERIALNO]
 
 Options:
-    -h --help   Show this screen
-    -c FILE     Specify config file [default: air.json]
-    -s SERIALNO Specify devices serialno(needed)
+    -h --help       Show this screen
+    -c FILE         Specify config file [default: air.json]
+    -s SERIALNO     Specify devices serialno(needed)
+    --skip-install  Skip install apk [default: false]
 '''
 
 import json
@@ -39,7 +40,7 @@ def urlretrieve(url, filename=None):
     print 'DOWNLOAD:', url, '->', filename
     return urllib.urlretrieve(url, filename)
 
-def run_android(jsonfile, serialno):
+def run_android(jsonfile, serialno, skip_install=False):
     d = json.load(open(jsonfile, 'r'))
     def xpath(*paths):
         v=d
@@ -50,29 +51,31 @@ def run_android(jsonfile, serialno):
     activity = xpath('android', 'activity')
     apk_url = xpath('android', 'apk_url')
 
-    urlretrieve(apk_url, 'test.apk')
     env = {'SERIALNO': serialno}
-    exec_cmd('adb', '-s', serialno, 'install', '-r', 'test.apk')
-    try:
+    if not skip_install:
+        urlretrieve(apk_url, 'test.apk')
+        exec_cmd('adb', '-s', serialno, 'install', '-r', 'test.apk')
         exec_cmd('adb', 'shell', 'am', 'start', '-n', '/'.join([package, activity]), timeout=10)
-        exec_cmd('bash', '-c', xpath('cmd'), env=env)
-    finally:
-        exec_cmd('adb', '-s', serialno, 'uninstall', package)
+    exec_cmd('bash', '-c', xpath('cmd'), env=env)
+    #try:
+    #finally:
+    #exec_cmd('adb', '-s', serialno, 'uninstall', package)
+    #pass
 
 def main():
     arguments = docopt(__doc__, version='0.1')
+    print 'ARGUMENTS:', arguments
     cnf = arguments.get('-c')
     if not os.path.exists(cnf):
         print 'Can not found conf file: %s' %(cnf)
         sys.exit(1)
-    print 'ARGUMENTS:', arguments
-    serialno = arguments.get('-s', '10.242.74.241:5555')
+    serialno = arguments.get('-s')
     exec_cmd('adb', 'start-server', timeout=10)
     out = subprocess.check_output(['adb', '-s', serialno, 'get-state'])
     if out.strip() != 'device': 
         print 'device(%s) not ready, current state:%s' %(serialno, out.strip())
         sys.exit(2)
-    run_android(cnf, serialno)
+    run_android(cnf, serialno, skip_install=arguments.get('--skip-install'))
 
 if __name__ == '__main__':
     main()
