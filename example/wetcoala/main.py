cb5d674fed1681715e493cfa4d4aa4feee00cdd2
game-netease <625368533@qq.com>
2014-07-02 10:49:16 +0800

    update ios example

--- example/wetcoala/main.py
+++ example/wetcoala/main.py
@@ -4,13 +4,13 @@ from airtest import ios
 def main():
     d = ios.IosDevice()
     d.click("start.png")
+    width, height = d.shape()
     for i in range(10):
         print "retry:", i
-        d.drag((160, 300), (290, 300))
-        print d.exists("retry.png")
-        d.drag((290, 300), (160, 300))
-        d.drag((160, 300), (30, 300))
-        d.wait("retry.png", interval=2)
+        d.drag((width * 0.5, height * 0.5), (width * 0.9, height * 0.5))
+        d.drag((width * 0.9, height * 0.5), (width * 0.5, height * 0.5))
+        d.drag((width * 0.5, height * 0.5), (width * 0.1, height * 0.5))
+        d.wait("gameover.png", interval=2)
         if d.exists("retry.png"):
             d.click("retry.png")
 
