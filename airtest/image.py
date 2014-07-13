fa07f139075284d283d5f668e41269d1be9c4204
JQH218 <jqh218@gmail.com>
2014-07-13 18:59:07 +0800

    add two  similarity claculation function to the image.py

--- airtest/image.py
+++ airtest/image.py
@@ -8,7 +8,11 @@ import time
 import math
 
 MIN_MATCH_COUNT = 5
-
+# Euclidean distance calculation
+def distance(p1, p2):
+    l2 = (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1])
+    return math.sqrt(l2)
+# remove the duplicate element of the list
 def reremove(list):
     ''' order preserving '''
     checked = []
@@ -16,7 +20,84 @@ def reremove(list):
         if e not in checked:
             checked.append(e)
     return checked
-
+# color hist based similarity calculation
+def HistSimilarity(origin='origin.png',query='query.png'):
+    img1 = cv2.imread(origin,1) # queryImage,gray
+    img2 = cv2.imread(query,1) # originImage,gray
+    try:
+        if img1.ndim ==2 & img2.ndim ==2:
+            hist1 = cv2.calcHist([img1],[0],None,[256],[0.0,255.0])
+            hist2 = cv2.calcHist([img2],[0],None,[256],[0.0,255.0])
+            retal = cv2.compareHist(hist1,hist2,cv2.cv.CV_COMP_CORREL)
+            elif img1.ndim ==3 & img2.ndim ==3:
+            # R,G,B split
+            b1,g1,r1 = cv2.split(img1)
+            b2,g2,r2 = cv2.split(img2)
+            hist_b1 = cv2.calcHist([b1],[0],None,[256],[0.0,255.0])
+            hist_g1 = cv2.calcHist([g1],[0],None,[256],[0.0,255.0])
+            hist_r1 = cv2.calcHist([r1],[0],None,[256],[0.0,255.0])
+            hist_b2 = cv2.calcHist([b2],[0],None,[256],[0.0,255.0])
+            hist_g2 = cv2.calcHist([g2],[0],None,[256],[0.0,255.0])
+            hist_r2 = cv2.calcHist([r2],[0],None,[256],[0.0,255.0])
+            retal_b = cv2.compareHist(hist_b1,hist_b2,cv2.cv.CV_COMP_CORREL)
+            retal_g = cv2.compareHist(hist_g1,hist_g2,cv2.cv.CV_COMP_CORREL)
+            retal_r = cv2.compareHist(hist_r1,hist_r2,cv2.cv.CV_COMP_CORREL)
+            sum_bgr = retal_b+retal_g+retal_r
+            retal = sum_bgr/3
+        else:
+            img1 = cv2.cvtColor(img1,cv2.cv.CV_RGB2Gray)
+            img2 = cv2.cvtColor(img2,cv2.cv.CV_RGB2Gray)
+            hist1 = cv2.calcHist([img1],[0],None,[256],[0.0,255.0])
+            hist2 = cv2.calcHist([img2],[0],None,[256],[0.0,255.0])
+            retal = cv2.compareHist(hist1,hist2,cv2.cv.CV_COMP_CORREL)
+	#retal = cv2.compareHist(hist1,hist2,cv2.cv.CV_COMP_BHATTACHARYYA)#Bhattacharyya distance
+	#retal = cv2.compareHist(hist1,hist2,cv2.cv.CV_COMP_CORREL)#Correlation
+	#retal = cv2.compareHist(hist1,hist2,cv2.cv.CV_COMP_CHISQR)#Chi-Square
+	#retal = cv2.compareHist(hist1,hist2,cv2.cv.CV_COMP_INTERSECT)#Intersection
+        return retal
+    except:
+        return None
+# SIFT or SURF based similarity calculation
+def FeatureSimilarity(origin='origin.png',query='query.png'):
+    img1 = cv2.imread(query,0) # queryImage,gray
+    img2 = cv2.imread(origin,0) # originImage,gray
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
+    #surf = cv2.SURF()
+    try:
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
+        #kp1, des1 = surf.detectAndCompute(img1,None)
+        #kp2, des2 = surf.detectAndCompute(img2,None)
+    except:
+        return []
+    kpnum1 = len(kp1)
+    kpnum2 = len(kp2)
+    print kpnum1,kpnum2
+	if kpnum1 <= kpnum2:
+        kpnum = kpnum1
+    else:
+        kpnum = kpnum2
+    print kpnum
+    if kpnum <= 0:
+		retal = 0.0
+		return retal
+    #search the match keypoints
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1,des2,k=2)
+	
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance: # threshold = 0.7
+            good.append(m)
+    kpnum_good = float(len(good))
+    print "Good Num: ", kpnum_good
+    retal = kpnum_good/kpnum
+    return retal
 def locate_image(orig,quer,outfile='debug.png',threshold=0.3):
     pt = locate_one_image(orig, quer, outfile, threshold)
     if pt:
@@ -144,15 +225,21 @@ def locate_one_image(origin='origin.png',query='query.png',outfile='match.png',t
         rcenter = [0,0]
         rcount = 0
         for i in range(count):
-            dis = abs(center_x-rlist_x[i])+abs(center_y-rlist_y[i])
+            #dis = abs(center_x-rlist_x[i])+abs(center_y-rlist_y[i])
+            dis = distance(temp,[rlist_x[i],rlist_y[i]])
             if max < dis:
                 max = dis
             dist.append(dis)
         for i in range(count):
-            if dist[i] < max:
-                rcenter[0] += rlist_x[i]
-                rcenter[1] += rlist_y[i]
-                rcount += 1
+            if count <= 2:
+				rcenter[0] += rlist_x[i]
+				rcenter[1] += rlist_y[i]
+				rcount += 1
+            else:
+                if dist[i] < max:
+                   rcenter[0] += rlist_x[i]
+                   rcenter[1] += rlist_y[i]
+                   rcount += 1
         if rcount < 1:
             print "NO Match"
             if outfile:
