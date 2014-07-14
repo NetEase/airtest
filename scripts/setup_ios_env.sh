1b7da6dab6bca5da160bb432f0efd8a43016a944
brantxiong <hustxiongbo@gmail.com>
2014-07-14 17:03:45 +0800

    fix setup_ios_env.sh

--- scripts/setup_ios_env.sh
+++ scripts/setup_ios_env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 confirm () {
     # call with a prompt string or use a default
     read -r -p "${1:-Are you sure? [y/N]} " response
@@ -14,6 +13,11 @@ confirm () {
     esac
 }
 
+
+is_in_brew() {
+    brew ls --versions $1 &>/dev/null && true || false
+}
+
 echo "!!!!!!!!!!WARNING!!!!!!!!!!!!!!"
 echo "DONOT run this script with root privilege"
 echo "Starting setup ios environment for airtest..."
@@ -24,26 +28,23 @@ confirm || exit 1
 echo "This may take a long time..."
 
 echo "-------Step 1: installing brew"
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew -v &>/dev/null && echo "brew already installed...skiping..." || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 echo "-------Step 2: installing a brewed python"
 brew tap homebrew/science
 brew tap homebrew/python
 brew update && brew upgrade
-brew install python
+is_in_brew python && echo "brewed python already installed...skiping..." ||  brew install python
 
 echo "-------Step 3: installing appium"
-brew install node
-npm install -g appium
+is_in_brew node && echo "node already installed...skiping..."  || brew install node
+appium -v &>/dev/null && echo "appium already installed...skiping..."  ||  npm install -g appium
 
 echo "-------Step 4: installing opencv"
-brew install opencv
+is_in_brew opencv && echo "opencv already installed...skiping..." || brew install opencv
 
 echo "-------Step 5: installing pillow"
-brew install pillow
-
-echo "-------Step 6: adding python environment"
-echo 'export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages' >> ~/.bash_profile
+is_in_brew pillow && echo "pillow already installed...skiping..." ||brew install pillow
 
 echo "-------End: summary"
 echo "python: " `which python`
