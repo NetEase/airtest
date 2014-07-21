621f5a7b59ff334cb851f2f13f165e9edcc106f6
codeskyblue <codeskyblue@gmail.com>
2014-07-21 13:01:50 +0800

    fix runtest

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,3 +1,7 @@
 cd $(dirname $0)
-export PYTHONPATH=$(cd ../; pwd):$PYTHONPATH
-py.test -v -l
+export PYTHONPATH=$(cd ../; pwd) #:$PYTHONPATH
+#export PYTHONPATH=$(cd ../;pwd)
+echo $PYTHONPATH
+#export PYTHONPATH=
+python -c 'import airtest; print airtest.__version__'
+py.test -v -l -s
