44ee7c211ad6d6ae47ef092c4a812380c84e5d66
codeskyblue <codeskyblue@gmail.com>
2014-08-05 11:23:41 +0800

    update test script

--- test/runtest.sh
+++ test/runtest.sh
@@ -3,6 +3,6 @@
 set -eu
 cd $(dirname $0)
 export PYTHONPATH=$(cd ../; pwd) #:$PYTHONPATH
-echo "DEBUG: pythonpath=$PYTHONPATH"
+echo "DEBUG: PYTHONPATH=$PYTHONPATH"
 python -c 'import airtest; print airtest.__version__'
-py.test -v -l -s "$@"
+py.test -v -l "$@"
