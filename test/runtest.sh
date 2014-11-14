e10dafc1f91cd26dc060d50261456efc0a4bfa7c
codeskyblue <codeskyblue@gmail.com>
2014-11-14 16:34:59 +0800

    add pythonpath for opencv

--- test/runtest.sh
+++ test/runtest.sh
@@ -2,7 +2,10 @@
 #
 set -eu
 cd $(dirname $0)
-export PYTHONPATH=$(cd ../; pwd) #:$PYTHONPATH
+
+PYTHONPATH=$(cd ../; pwd):/usr/lib/pyshared/python2.7:$PYTHONPATH
+export PYTHONPATH
+
 echo "DEBUG: PYTHONPATH=$PYTHONPATH"
 python -c 'import airtest; print airtest.__version__'
 py.test -v -l "$@"
