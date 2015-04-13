9ff8f0fd32191a5e874c4d94eee6d95c220b3029
codeskyblue <codeskyblue@gmail.com>
2015-04-13 17:08:21 +0800

    add install.py for windows

--- publish.sh
+++ publish.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -
 #
 # 
-python setup.py sdist upload -r ${1:-privatepypi}
+set -eu
+cd $(dirname $0)
+rm -fr dist
+python setup.py sdist
+
+# Sync to qiniu
+mv dist/*.tar.gz qiniu/files/airtest.tar.gz
+qiniu/sync.sh
+
+# Sync to pypi
+python setup.py upload -r ${1:-pypi}
---
+++ scripts/install.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+#
+'''
+To install airtest, download get-airtest.py
+
+    python get-airtest.py
+
+Many requirements will be install automaticly.
+
+    pycrypto-2.6.win32-py2.7
+    pyparsing-2.0.2.win32-py2.7
+    numpy-MKL-1.8.1.win32-py2.7
+    python-dateutil-2.2.win32-py2.7
+    matplotlib-1.3.1.win32-py2.7
+    Pillow-2.4.0.win32-py2.7
+    pip-1.5.6.win32-py2.7
+    setuptools-5.4.2.win32-py2.7
+    opencv-python-2.4.9.win32-py2.7
+'''
+
+
+import sys
+import urllib
+import platform
+
+
+def log(msg):
+    print '>>> LOG:', msg
+
+
+def err(msg):
+    print '>>> ERR:', msg
+    sys.exit(1)
+
+
+# Check
+if not platform.system() == 'Windows':
+    err('Script only support windows platform')
+
+pyver = platform.python_version()
+if not pyver.startswith('2.7'):
+    err('Need python version 2.7.*, not %s' % (pyver))
+
+try:
+    import pip
+except:
+    log('Install pip')
+    code = urllib.urlopen('https://bootstrap.pypa.io/get-pip.py').read()
+    exec code
+    import pip
+
+installed = {}
+for soft in pip.pip.get_installed_distributions():
+    installed[soft.project_name] = soft.version
+
+CDN_PREFIX = 'http://goandroid.qiniudn.com/airtest/'
+
+requirements = [
+    ['pyparsing', '2.0.2', '2-pyparsing-2.0.2.win32-py2.7.exe'],
+    ['numpy', '1.8.1', '3-numpy-MKL-1.8.1.win32-py2.7.exe'],
+    ['python-dateutil', '2.2', '4-python-dateutil-2.2.win32-py2.7.exe'],
+    ['matplotlib', '1.3.1', '5-matplotlib-1.3.1.win32-py2.7.exe'],
+    ['pillow', '2.4.0', '6-Pillow-2.4.0.win32-py2.7.exe'],
+    ['opencv-python', '2.4.9', '9-opencv-python-2.4.9.win32-py2.7.exe'],
+    ['pycrypto', '2.6', '10-pycrypto-2.6.win32-py2.7.exe']
+]
+    #['setuptools', '5.4.2', '8-setuptools-5.4.2.win32-py2.7.exe'],
+
+import subprocess
+for name, ver, fname in requirements:
+    if installed.get(name) == ver:
+        continue
+    print installed.get(name), ver, name
+    p = subprocess.Popen(['easy_install', CDN_PREFIX+fname])
+    p.wait()
